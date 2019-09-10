ae815aa828ed80f22434b9c6fc9fbdc3062d0785
Евгений Кривоносов <e.krivonosov@digital-sector.ru>
2019-09-10 23:15:45 +0300

    template update

--- docker_start/local/templates/main/header.php
+++ docker_start/local/templates/main/header.php
@@ -6,19 +6,5 @@ IncludeTemplateLangFile(__FILE__);
 <?php $APPLICATION->ShowHead();?>
 <title><?php $APPLICATION->ShowTitle()?></title>
 </head>
-
-<body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" bgcolor="#FFFFFF">
-
+<body>
 <?php $APPLICATION->ShowPanel()?>
-
-<?php if($USER->IsAdmin()):?>
-
-<div style="border:red solid 1px">
-	<form action="/bitrix/admin/site_edit.php" method="GET">
-		<input type="hidden" name="LID" value="<?=SITE_ID?>" />
-		<p><font style="color:red"><?= GetMessage("T_MAIN_TEMPLATE_NF")?> </font></p>
-		<input type="submit" name="set_template" value="<?= GetMessage("T_MAIN_TEMPLATE_NF_SET")?>" />
-	</form>
-</div>
-
-<?php endif?>
