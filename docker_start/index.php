4be423b6426b0a304a034eaecf98d7858587a1f5
acca <accalmao@mail.ru>
2022-08-16 15:07:10 +0300

    init

--- docker_start/index.php
+++ docker_start/index.php
@@ -5,18 +5,30 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
  */
 $APPLICATION->SetTitle("main page");
 $APPLICATION->IncludeComponent(
-    "photoalbum:album.list",
+    "tokmakov:iblock.element",
     "",
-    [
+    Array(
+        "ADD_SECTIONS_CHAIN" => "Y",
+        "CACHE_GROUPS" => "Y",
         "CACHE_TIME" => "3600",
         "CACHE_TYPE" => "A",
-        "IBLOCK_CODE" => 'photoalbum',
-        "IBLOCK_TYPE" => 'album',
-    ]
+        "ELEMENT_CODE" => $_REQUEST["ELEMENT_CODE"],
+        "ELEMENT_ID" => $_REQUEST["ELEMENT_ID"],
+        "ELEMENT_URL" => "item/id/#ELEMENT_ID#/",
+        "FILE_404" => "",
+        "IBLOCK_ID" => "5",
+        "IBLOCK_TYPE" => "content",
+        "MESSAGE_404" => "",
+        "SECTION_URL" => "category/id/#SECTION_ID#/",
+        "SET_BROWSER_TITLE" => "Y",
+        "SET_META_DESCRIPTION" => "Y",
+        "SET_META_KEYWORDS" => "Y",
+        "SET_PAGE_TITLE" => "Y",
+        "SET_STATUS_404" => "Y",
+        "SHOW_404" => "Y",
+        "USE_CODE_INSTEAD_ID" => "N"
+    )
 );
-echo "<pre>";
-print_r($arResult);
-echo "</pre>";
 ?>
 
 
